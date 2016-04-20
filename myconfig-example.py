5a6891fe46f07ca60cd9a2041c3fe46637a31fbe
Tomas <tomas@criptos.com>
2016-04-20 19:38:28 +0200

    Adding another API to myconfig

--- myconfig-example.py
+++ myconfig-example.py
@@ -1,2 +1,3 @@
 # Get your OpenWeatherMap API ID at http://openweathermap.org/api
 OpenWeatherMapAPIToken = 'Get your API ID at http://openweathermap.org/api'
+forecastioAPIKey = 'Get your API ID at https://developer.forecast.io/'
